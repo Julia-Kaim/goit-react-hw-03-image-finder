c9d372dd098cc2925dc2d4cfeae06ba263e9ada7
Julia-Kaim <julia.kaim08@gmail.com>
2023-05-25 10:25:41 +0200

    fix: asyc/await

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -19,34 +19,65 @@ export default class App extends Component {
     totalHits: null,
   };
 
-  fetchImg = () => {
-    return fetch(
-      `${this.state.URL}?q=${this.state.query}&page=${this.state.page}&key=${this.state.API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
-    )
-      .then(res => {
-        if (res.ok) {
-          return res.json();
-        }
-        return Promise.reject(new Error('Failed to find any images'));
-      })
-      .then(pictures => {
+  // fetchImg = () => {
+  //   return fetch(
+  //     `${this.state.URL}?q=${this.state.query}&page=${this.state.page}&key=${this.state.API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+  //   )
+  //     .then(res => {
+  //       if (res.ok) {
+  //         return res.json();
+  //       }
+  //       return Promise.reject(new Error('Failed to find any images'));
+  //     })
+  //     .then(pictures => {
+  //       if (!pictures.total) {
+  //         toast.error('Did find anything, mate');
+  //       }
+  //       const selectedProperties = pictures.hits.map(
+  //         ({ id, largeImageURL, webformatURL }) => {
+  //           return { id, largeImageURL, webformatURL };
+  //         }
+  //       );
+  //       this.setState(prevState => {
+  //         return {
+  //           pictures: [...prevState.pictures, ...selectedProperties],
+  //           status: 'resolved',
+  //           totalHits: pictures.total,
+  //         };
+  //       });
+  //     })
+  //     .catch(error => this.setState({ error, status: 'rejected' }));
+  // };
+  fetchImg = async () => {
+    try {
+      const response = await fetch(
+        `${this.state.URL}?q=${this.state.query}&page=${this.state.page}&key=${this.state.API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+      );
+
+      if (response.ok) {
+        const pictures = await response.json();
+
         if (!pictures.total) {
-          toast.error('Did find anything, mate');
+          toast.error('Did not find anything, mate');
         }
+
         const selectedProperties = pictures.hits.map(
           ({ id, largeImageURL, webformatURL }) => {
             return { id, largeImageURL, webformatURL };
           }
         );
-        this.setState(prevState => {
-          return {
-            pictures: [...prevState.pictures, ...selectedProperties],
-            status: 'resolved',
-            totalHits: pictures.total,
-          };
-        });
-      })
-      .catch(error => this.setState({ error, status: 'rejected' }));
+
+        this.setState(prevState => ({
+          pictures: [...prevState.pictures, ...selectedProperties],
+          status: 'resolved',
+          totalHits: pictures.total,
+        }));
+      } else {
+        throw new Error('Failed to find any images');
+      }
+    } catch (error) {
+      this.setState({ error, status: 'rejected' });
+    }
   };
 
   componentDidUpdate(prevProps, prevState) {
