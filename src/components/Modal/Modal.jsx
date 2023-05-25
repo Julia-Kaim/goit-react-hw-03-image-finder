91905c24f6312ee9280c76bdbd928665024c254c
Julia-Kaim <julia.kaim08@gmail.com>
2023-05-25 11:00:12 +0200

    fix: addEventListener change on Modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,36 +1,65 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useCallback } from 'react';
 
 import PropTypes from 'prop-types';
 
 import styles from './Modal.module.css';
 
-export default class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
+// export default class Modal extends Component {
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.handleKeyDown);
+//   }
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.handleKeyDown);
+//   }
 
-  handleKeyDown = e => {
-    if (e.code === 'Escape') {
-      return this.props.onClose();
-    }
-  };
+//   handleKeyDown = e => {
+//     if (e.code === 'Escape') {
+//       return this.props.onClose();
+//     }
+//   };
 
-  render() {
-    return (
-      <div className={styles.overlay}>
-        <div className={styles.modal}>
-          <img src={this.props.pic} alt="" />
-        </div>
+//   render() {
+//     return (
+//       <div className={styles.overlay}>
+//         <div className={styles.modal}>
+//           <img src={this.props.pic} alt="" />
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+const Modal = ({ pic, onClose }) => {
+  const handleKeyDown = useCallback(
+    e => {
+      if (e.code === 'Escape') {
+        onClose();
+      }
+    },
+    [onClose]
+  );
+
+  useEffect(() => {
+    const eventListener = e => handleKeyDown(e);
+    window.addEventListener('keydown', eventListener);
+    return () => {
+      window.removeEventListener('keydown', eventListener);
+    };
+  }, [handleKeyDown]);
+
+  return (
+    <div className={styles.overlay}>
+      <div className={styles.modal}>
+        <img src={pic} alt="" />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 Modal.propTypes = {
   onClose: PropTypes.func.isRequired,
   pic: PropTypes.string.isRequired,
 };
+
+export default Modal;
